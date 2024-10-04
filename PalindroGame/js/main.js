d511578d3309f482c2a34ea3e8e4a85c7b18bc76
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-04 14:27:07 +0200

    Add file e comments

---
+++ PalindroGame/js/main.js
@@ -0,0 +1,6 @@
+console.log("it Works");
+
+//Chiedere all’utente di inserire una parola
+// Creare una funzione --> OBIETTIVO: per capire se la parola inserita è palindroma;
+//è necessario estrarre l'indice della parola
+
---
+++ Pari-o-dispari/js/main.js
@@ -0,0 +1,22 @@
+//Azioni utente:
+//1 - scelta pari o dispari
+//2 - inserimento numero da 1 a 5;
+
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). --> Math.floor(Math.random())
+//- Loop for?
+//- array?
+//- function ?
+
+
+
+//Sommiamo i due numeri Stabiliamo:
+// SE la somma dei due numeri è pari -->
+// SE la somma dei numeri è dispari -->
+//-sum
+//- %
+//- if, else if, else
+
+
+//Dichiariamo chi ha vinto
+//- Alert 
