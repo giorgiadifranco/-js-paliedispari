b5b9e1d7df5460404b4bbee358656b11d1894a1c
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-04 17:43:39 +0200

    add function

--- Pari-o-dispari/js/main.js
+++ Pari-o-dispari/js/main.js
@@ -1,11 +1,42 @@
 //Azioni utente:
-//1 - scelta pari o dispari
-//2 - inserimento numero da 1 a 5;
+//1 - scelta pari o dispari --> questo dato dovrebbe essere salvato in una lista temporanea al fine di fare la verifica"
+//2 - inserimento numero da 1 a 5 --> anche questo dato dovrà essere salvato in un array temporaneo al fine di essere utilizzato;
 
-let evenOdd = prompt('Ciao, giochiamo a "Pari&Dispari", cosa scegli?');
-console.log(evenodd);
+let evenOdd = []
+//let userChoice; //= prompt('Ciao, giochiamo a "Pari&Dispari", cosa scegli?');
+//console.log(evenOdd);
 
-userChoice = evenOdd.push();
+let number = [];
+
+//let userDecision = evenOdd.push();
+//console.log(userChoice);
+function pariOdispari(evenOdd, number){
+    let userChoice = prompt('Ciao, giochiamo a "Pari&Dispari", cosa scegli?');
+    evenOdd.push(userChoice); //può memorizzare solo una parola o pari o dispari
+
+    console.log(evenOdd);
+    
+    let userNumber = prompt ("Adesso, inserisci un numero da 1 a 5");
+    console.log(userNumber);
+    let convertNumber = userNumber.parseInt()
+    console.log(convertNumber);
+    number.push(convertNumber);
+    console.log(number)
+
+ 
+} console.log(pariOdispari(evenOdd, number))
+
+
+
+
+
+let userNumber = prompt ("Adesso, inserisci un numero da 1 a 5");
+console.log(userNumber);
+
+
+
+
+
 
 
 
