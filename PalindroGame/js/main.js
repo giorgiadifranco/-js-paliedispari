31dcdcd8fcca89814a8869bc640f591a4d9c8c6e
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-04 15:01:37 +0200

    Add function

--- PalindroGame/js/main.js
+++ PalindroGame/js/main.js
@@ -1,6 +1,36 @@
 console.log("it Works");
 
 //Chiedere all’utente di inserire una parola
+let wordInput = prompt("Ciao! inserisci una parola:");
+
+console.log(wordInput);
+
 // Creare una funzione --> OBIETTIVO: per capire se la parola inserita è palindroma;
+//- reverse
+
+
+
+function palindoGame (){
+
+
+let wordSchizo = wordInput.split('');
+console.log(wordSchizo);
+
+let wordReverse = wordSchizo.reverse();
+console.log(wordReverse);
+
+let wordJoin = wordSchizo.join('');
+
+return wordJoin
+}
+
+palindoGame()
+
+
+
+
+// Creare una funzione --> OBIETTIVO: per capire se la parola inserita è palindroma;
+//- reverse
+
 //è necessario estrarre l'indice della parola
 
